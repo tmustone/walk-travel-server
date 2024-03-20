86dbe532fcd75f38428f292ecf0b050af3e69955
Tommi Mustonen <tommi_mustonen@hotmail.com>
2024-03-20 22:30:23 +0200

    Added authentication server information to configuration

--- src/main/java/net/dynu/wpeckers/walktraveler/configuration/AuthenticationConfiguration.java
+++ src/main/java/net/dynu/wpeckers/walktraveler/configuration/AuthenticationConfiguration.java
@@ -1,21 +1,37 @@
 package net.dynu.wpeckers.walktraveler.configuration;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import net.duny.wpeckers.authentication.client.AuthenticationClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
 @Configuration
+@ConfigurationProperties(prefix = "authentication")
+@Data
 public class AuthenticationConfiguration {
 
-    public static final String SERVICE_NAME = "walktraveller-int.wpeckers.dynu.net";
+    private String serviceName;
+    private String url;
+
+    @PostConstruct
+    public void init() {
+        log.info("Authentication configuration:");
+        log.info("url : {}", this.url);
+        log.info("serviceName: {}", this.serviceName);
+    }
 
     @Bean
     public AuthenticationClient getAuthenticationClient() {
-        return new AuthenticationClient(AuthenticationClient.BASE_URL_INT);
+        return new AuthenticationClient(this.url);
     }
 
     public String getServiceName() {
-        return SERVICE_NAME;
+        return this.serviceName;
     }
 
 }
